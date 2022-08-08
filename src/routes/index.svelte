ac298282576d41ef743915592933c8d24385d043
dromzeh <65048232+dromzeh@users.noreply.github.com>
2022-08-08 02:25:38 +0100

    read all projects from projects.json

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,11 @@
+<script context = "module">
+import data from '../data/projects.json';
+</script>
+
+<script>
+let projects = data.projects;
+</script>
+
 <svelte:head>
 <title>dromzeh.dev</title>
 </svelte:head>
@@ -6,11 +14,10 @@
   <div class = "move-area mx-auto -mt-24 flex min-h-screen flex-1 flex-col duration-300 motion-reduce:transition-none md:w-[90%] xl:w-4/5">
   <div class = "md:grid-cols-0 grid px-8 lg:grid-cols-5">
     <div class = "md:col-span-3">
-
-      <h4 class = "text-2xl font-semibold text-white underline decoration-gray-500 hover:decoration-sky-200 decoration-4">hi 👋!</h4>
-      <div class = "py-2">
+    <h4 class = "text-2xl font-semibold text-white underline decoration-gray-500 hover:decoration-sky-200 decoration-4">hi 👋!</h4>
+    <div class = "py-2">
       <p class = "text-white text-l"> i'm a full stack developer & i'm also the creator and sole developer of <a href = "https://wanderer.moe" class = "hover:bg-opacity-25 bg-sky-300 bg-opacity-5 p-1 rounded-md font-bold text-sky-200">wanderer.moe</a>. <br> (the genshin site you're probably looking for)</p>
-      </div>
+    </div>
     <br>
   
     <h4 class = "text-2xl font-semibold text-white underline decoration-gray-500 hover:decoration-sky-200 decoration-4">contact info</h4>
@@ -47,100 +54,32 @@
   </div>
   
   <div class = "motion-reduce:transition-none lg:col-span-2 lg:mt-0 lg:mb-0">
-
   <h4 class="text-2xl font-semibold text-white underline decoration-gray-500 hover:decoration-sky-200 decoration-4">some projects i've created</h4>
-
   <div class = "py-2">
   <div class="grid grid-cols-1 gap-4">
-
+  {#each projects as project}
   <div class="container mx-auto">
-    <a href="https://wanderer.moe">
+    <a href="{project.url}">
     <div class="bg-stone-800 projectContainer transition duration-150 ease-in-out rounded p-4 shadow md:flex justify-between hover:scale-105">
       <div>
-        <h4 class="text-2xl font-semibold text-white">wanderer.moe</h4>
-        <p class="my-2 text-sm text-white indent-2">A website which hosts <span class = "text-sky-200">hundreds of Genshin Impact assets</span>. Averages over <span class = "text-sky-200">30,000</span> individual users per month. (and a lot of bandwidth usage 😵‍💫) </p>
-
+        <h4 class="text-2xl font-semibold text-white">{project.name}</h4>
+        <p class="my-2 text-sm text-white indent-2">{project.description}</p>
         <div class="flex items-center mt-4 gap-2">
+          {#each project.uses as uses}
           <div class="hover:bg-opacity-25 bg-sky-300 bg-opacity-5 p-2 rounded-md font-bold uppercase text-sm text-sky-200">
-            Svelte
+            {uses}
           </div>
-          <div class="hover:bg-opacity-25 bg-sky-300 bg-opacity-5 p-2 rounded-md font-bold uppercase text-sm text-sky-200">
-            TailwindCSS
-          </div>
+          {/each}
         </div>
-
       </div>
     </div>
   </a>
-  </div> 
-
-  <div class="container mx-auto">
-    <a href="https://github.com/dromzeh/robloxsoundchanger">
-    <div class="bg-stone-800 projectContainer transition duration-150 ease-in-out rounded p-4 shadow md:flex justify-between hover:scale-105">
-      <div>
-        <h4 class="text-2xl font-semibold text-white">roblox sound changer</h4>
-        <p class="my-2 text-sm text-white indent-2">Windows program that allows you to easily modify your roblox 'death' sound. Also includes <span class = "text-sky-200">auto detection of game installation location</span> for easier usage.</p>
-
-        <div class="flex items-center mt-4 gap-2">
-          <div class="hover:bg-opacity-25 bg-sky-300 bg-opacity-5 p-2 rounded-md font-bold uppercase text-sm text-sky-200">
-            C#
-          </div>
-          <div class="hover:bg-opacity-25 bg-sky-300 bg-opacity-5 p-2 rounded-md font-bold uppercase text-sm text-sky-200">
-            WinForms
-          </div>
-        </div>
-        
-      </div>
-    </div>
-  </a>
-  </div> 
-
-  <div class="container mx-auto">
-    <a href="https://github.com/dromzeh/roblox-filter-for-discord">
-    <div class="bg-stone-800 projectContainer transition duration-150 ease-in-out rounded p-4 shadow md:flex justify-between hover:scale-105">
-      <div>
-        <h4 class="text-2xl font-semibold text-white">roblox filter for discord</h4>
-        <p class="my-2 text-sm text-white indent-2">A bot for discord which allows you to implement <span class = "text-sky-200">"roblox-type"</span> moderation into a server's channel, uses webhooks to <span class = "text-sky-200">mimic talking as the person.</span></p>
-
-        <div class="flex items-center mt-4 gap-2">
-          <div class="hover:bg-opacity-25 bg-sky-300 bg-opacity-5 p-2 rounded-md font-bold uppercase text-sm text-sky-200">
-            Python
-          </div>
-          <div class="hover:bg-opacity-25 bg-sky-300 bg-opacity-5 p-2 rounded-md font-bold uppercase text-sm text-sky-200">
-            Discord.Py
-          </div>
-        </div>
-        
-      </div>
-    </div>
-  </a>
-  </div> 
-
-  <div class="container mx-auto">
-    <a href="https://github.com/dromzeh/discord-custom-stream-status">
-    <div class="bg-stone-800 projectContainer transition duration-150 ease-in-out rounded p-4 shadow md:flex justify-between hover:scale-105">
-      <div>
-        <h4 class="text-2xl font-semibold text-white">discord custom stream status</h4>
-        <p class="my-2 text-sm text-white indent-2">Discord selfbot which allows for easily <span class = "text-sky-200">setting your status to streaming</span> and set it any name of your choice.</p>
-
-        <div class="flex items-center mt-4 gap-2">
-          <div class="hover:bg-opacity-25 bg-sky-300 bg-opacity-5 p-2 rounded-md font-bold uppercase text-sm text-sky-200">
-            Python
-          </div>
-          <div class="hover:bg-opacity-25 bg-sky-300 bg-opacity-5 p-2 rounded-md font-bold uppercase text-sm text-sky-200">
-            Discord.Py
-          </div>
-        </div>
-        
-      </div>
-    </div>
-  </a>
-  </div> 
-
-
   </div>
-</div>
-</div>
-</div>
-</div>
+  {/each}
+
+    </div>
+    </div>
+    </div>
+  </div>
+  </div>
 </div>
