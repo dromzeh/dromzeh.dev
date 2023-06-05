3fb01a65411d95cedba45c534858156ef62f8d29
Marcel <65048232+dromzeh@users.noreply.github.com>
2023-06-05 14:14:56 +0100

    set adapter to cloudflare, prerender posts
    ;3

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-cloudflare'
 import { vitePreprocess } from '@sveltejs/kit/vite'
 import { mdsvex, escapeSvelte } from 'mdsvex'
 import shiki from 'shiki'
