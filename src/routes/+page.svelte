2b5f69191d8f3dc2ef83434086818398a55d3ab3
Marcel <65048232+dromzeh@users.noreply.github.com>
2023-09-25 18:19:42 +0100

    fix title spacing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,8 +11,10 @@ import SocialsContainer from '../components/socials/SocialsContainer.svelte'
 <div class="py-24 text-zinc-400">
 	<div class="flex flex-col gap-3">
 		<div>
-			<h1 class="mb-2 text-4xl font-semibold text-white">dromzeh.dev</h1>
-			<Intro />
+			<h1 class="text-4xl font-semibold text-white">dromzeh.dev</h1>
+			<div class="mb-2 mt-8 flex flex-col gap-3">
+				<Intro />
+			</div>
 		</div>
 		<SocialsContainer />
 	</div>
