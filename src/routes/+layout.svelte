953fe0cb06554b580a324d87c14fa55e0ac65e9f
Marcel <65048232+dromzeh@users.noreply.github.com>
2023-10-29 20:57:46 +0000

    fly page transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,7 @@ import Footer from '../components/navigation/Footer.svelte'
 import 'iconify-icon'
 import NProgress from 'nprogress'
 import { afterNavigate, beforeNavigate } from '$app/navigation'
+import { fly } from 'svelte/transition'
 
 beforeNavigate(() => {
 	NProgress.start()
@@ -13,10 +14,18 @@ beforeNavigate(() => {
 afterNavigate(() => {
 	NProgress.done()
 })
+
+export let data
 </script>
 
-<Navbar />
-<div class="mx-auto min-h-screen max-w-[90ch] break-normal px-4 sm:px-6 lg:px-8">
-	<slot />
-</div>
-<Footer />
+<main>
+	<Navbar />
+	{#key data.url}
+		<div in:fly="{{ y: 20, duration: 300 }}">
+			<div class="mx-auto min-h-screen max-w-[90ch] break-normal px-4 sm:px-6 lg:px-8">
+				<slot />
+			</div>
+		</div>
+	{/key}
+	<Footer />
+</main>
