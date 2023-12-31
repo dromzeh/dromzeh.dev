ca7ae9c4138624320eba2b9b2aaed6171234fe6d
dromzeh <65048232+dromzeh@users.noreply.github.com>
2023-12-31 10:14:48 +0000

    add back age component

--- src/components/main/about.tsx
+++ src/components/main/about.tsx
@@ -1,10 +1,16 @@
 "use client";
 import Link from "next/link";
-
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipTrigger,
+    TooltipProvider,
+} from "../ui/tooltip";
+import dayjs from "dayjs";
 import * as React from "react";
 
-// const ageDateTimestamp = 1156118401000;
-// const fallBackAge = 17;
+const ageDateTimestamp = 1156118401000;
+const fallBackAge = 17;
 
 export function About() {
     React.useEffect(() => {
@@ -19,9 +25,9 @@ export function About() {
             <h2 className="text-neutral-100 mb-2">About</h2>
             <div className="flex flex-col space-y-2">
                 <p>
-                    Hey, I&apos;m Marcel - a full-stack developer focused within
-                    infrastructure, serverless computing and backend
-                    development.
+                    Hey, I&apos;m Marcel - <Age /> year old full-stack developer
+                    focused within infrastructure, serverless computing and
+                    backend development.
                 </p>
                 <p>
                     I&apos;m the creator of{" "}
@@ -87,5 +93,33 @@ export function About() {
                 </p>
             </div>
         </div>
+    );
+}
+
+export function Age() {
+    const [age, setAge] = React.useState<number>(fallBackAge);
+
+    React.useEffect(() => {
+        const intervalId = setInterval(() => {
+            setAge(dayjs().diff(ageDateTimestamp, "year", true));
+        }, 50);
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
+
+    return (
+        <TooltipProvider delayDuration={100}>
+            <Tooltip>
+                <TooltipTrigger asChild>
+                    <span className="text-neutral-100 hover:text-neutral-500 transition-all duration-150 cursor-pointer">
+                        {age.toFixed(4)}
+                    </span>
+                </TooltipTrigger>
+                <TooltipContent>
+                    <p>{age.toFixed(12)}</p>
+                </TooltipContent>
+            </Tooltip>
+        </TooltipProvider>
     );
 }
