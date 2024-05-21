0f05d343897c8178f39abb052f2146379ba2bcbc
Marcel <65048232+dromzeh@users.noreply.github.com>
2024-05-22 00:29:32 +0100

    true

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
                 <ThemeProvider attribute="class" defaultTheme="dark">
                     <CommandMenu />
                     <ThemeToggle />
-                    {children}
+                    <div className="my-4">{children}</div>
                     <Toaster />
                 </ThemeProvider>
             </body>
