7b11f83f59320f27e0618b4da98184942e57c593
Marcel Elias <marcel@dromzeh.dev>
2024-11-08 11:41:54 +0000

    feat: scramble lanyard status switch

--- src/components/main/lanyard.tsx
+++ src/components/main/lanyard.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 import type { Activity } from "react-use-lanyard";
 import { useEffect, useState } from "react";
 import Link from "next/link";
-
+import { TextScramble } from "../scramble";
 const statusMap = {
     online: "Online",
     dnd: "Do Not Disturb",
@@ -23,6 +23,8 @@ const statusColorMap = {
 
 export function LanyardProfile() {
     const [activity, setActivity] = useState<Activity | undefined>(undefined);
+    const [currentStatus, setCurrentStatus] = useState<any>(null);
+    const [triggerScramble, setTriggerScramble] = useState(false);
 
     const { loading, status } = useLanyard({
         userId: "492731761680187403",
@@ -37,9 +39,16 @@ export function LanyardProfile() {
         }
     }, [status]);
 
-    // if (loading || !status || !status.discord_user) {
-    //     return null;
-    // }
+    useEffect(() => {
+        if (status !== currentStatus) {
+            setTriggerScramble(true);
+            setCurrentStatus(status);
+        }
+    }, [status]);
+
+    const handleScrambleComplete = () => {
+        setTriggerScramble(false);
+    };
 
     return loading || !status || !status.discord_user ? (
         <div className="flex flex-row space-x-2 items-center">
@@ -50,23 +59,6 @@ export function LanyardProfile() {
         </div>
     ) : (
         <div className="flex flex-row space-x-2 items-center">
-            {/* <div className="relative mr-2">
-                <Image
-                    src={`https://cdn.discordapp.com/avatars/${status.discord_user.id}/${status.discord_user.avatar}`}
-                    alt="discord avatar"
-                    width={64}
-                    height={64}
-                    className="rounded-lg"
-                />
-                <span className="absolute bottom-0 right-0 translate-y-2 translate-x-2">
-                    <span
-                        className={`animate-ping absolute inline-flex size-4 rounded-full ${statusColorMap[status.discord_status]} opacity-75`}
-                    />
-                    <span
-                        className={`relative inline-flex rounded-full ${statusColorMap[status.discord_status]} size-4`}
-                    />
-                </span>
-            </div> */}
             <div className="flex flex-col space-y-2">
                 <p className="text-foreground">
                     Marcel Alexander Elias<span className="caret">_</span>
@@ -88,23 +80,26 @@ export function LanyardProfile() {
                                     height={48}
                                     className="size-5 rounded-md mr-1"
                                 />
-                                <p className="line-clamp-1 overflow-ellipsis">
-                                    Listening to{" "}
-                                    {status.spotify?.song.length! < 50 ? (
-                                        <Link
-                                            href={`https://open.spotify.com/track/${status.spotify?.track_id}`}
-                                            target="_blank"
-                                            className="text-foreground hover:cursor-pointer hover:text-muted-foreground/50 transition-all duration-150"
-                                        >
-                                            {status.spotify?.song}
-                                        </Link>
-                                    ) : (
-                                        <span> Spotify</span>
-                                    )}
-                                </p>
+                                <TextScramble
+                                    duration={0.2}
+                                    speed={0.04}
+                                    trigger={triggerScramble}
+                                    onScrambleComplete={handleScrambleComplete}
+                                >
+                                    {status.spotify?.song.length! < 50
+                                        ? `Listening to ${status.spotify?.song}`
+                                        : "Listening to Spotify"}
+                                </TextScramble>
                             </div>
                         ) : (
-                            <p>{statusMap[status.discord_status]} on Discord</p>
+                            <TextScramble
+                                duration={0.2}
+                                speed={0.04}
+                                trigger={triggerScramble}
+                                onScrambleComplete={handleScrambleComplete}
+                            >
+                                {`${statusMap[status.discord_status]} on Discord`}
+                            </TextScramble>
                         )}
                     </div>
                 </div>
